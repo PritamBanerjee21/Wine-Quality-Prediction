ca67b02342ea4392a2eb8fcb0b61e053b2b954de
PritamBanerjee21 <169151494+PritamBanerjee21@users.noreply.github.com>
2024-07-06 11:39:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ y = df['quality']
 
 #importing dependencies
 from imblearn.ensemble import BalancedRandomForestClassifier
-from sklearn.model_selection import train_test_split
+from scikit-lean.model_selection import train_test_split
 
 #Splitting the dataset
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
